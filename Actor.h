167f0f9e0178ca757abad49a629be5e2b9724837
xinshoujiajia <62921971+xinshoujiajia@users.noreply.github.com>
2021-01-08 18:04:35 +0800

    Update Actor.h

--- Actor.h
+++ Actor.h
@@ -10,9 +10,16 @@ private:
   int mColor;
   class Game* mGame;
 public:
+  Actor(Game* game);
+  ~Actor();
   class Game* GetGame(){return mGame;}
+  
+  void SetName(string name){mName=name;}
   string GetName(){return mName;}
+  
+  void SetColor(int color){mColor=color;}
   int GetColor(){return mColor;}
+  
   void Talk(Actor* actor);
   void Draw();
 }
