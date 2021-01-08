4294f89c7daec54298513497a7b94c46b1188ff3
xinshoujiajia <62921971+xinshoujiajia@users.noreply.github.com>
2021-01-08 17:51:35 +0800

    Update Actor.h

--- Actor.h
+++ Actor.h
@@ -1,5 +1,18 @@
 #pragma once
+#include<string.h>
 class Actor
 {
-
+private:
+  int x;
+  int y;
+  string mName;
+  string mIcon;
+  int mColor;
+  class Game* mGame;
+public:
+  class Game* GetGame(){return mGame;}
+  string GetName(){return mName;}
+  int GetColor(){return mColor;}
+  void Talk(Actor* actor);
+  void Draw();
 }
