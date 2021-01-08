32d0a6cf76132e04ba6345b988b746bcfc81b513
xinshoujiajia <62921971+xinshoujiajia@users.noreply.github.com>
2021-01-08 18:01:59 +0800

    Update Actor.cpp

--- Actor.cpp
+++ Actor.cpp
@@ -1,1 +1,29 @@
+#include"Actor.h"
+#include<Windows.h>
+#include<iostream>
 
+void SetColor(int x){
+	HANDLE h = GetStdHandle (-11);
+	SetConsoleTextAttribute(h,x);
+}
+
+void Actor::Draw()
+{
+  SetColor(mColor);
+  std::cout<<mIcon;
+}
+
+void Actor::Talk(Actor* actor)
+{
+  actor->SetPosition(x,y);
+}
+
+Actor::Actor(Game* game)
+  :x(0)
+  ,y(0)
+  ,mColor(15)
+  ,mIcon("  ")
+  ,mName("None")
+{
+  
+}
